887eb88ef37d42aa5c64a65c69458e230fedfd7a
PeterGillow <peter.gillow.krooni@gmail.com>
2022-03-09 17:03:20 +0100

    made some improvements on nav menu

--- src/frontend-v2/src/components/NavMenu.vue
+++ src/frontend-v2/src/components/NavMenu.vue
@@ -1,9 +1,10 @@
 <template> 
     <div id="menu">
         <div id="box">
-            <Button class='menu-btn' @click="logOut" :text='"Log out"' /> 
-            <Button class='menu-btn' @click="this.$router.push('/profile')" :text='"Profile"' /> 
-            <Button class='menu-btn' @click="logOut" :text='"➕Add movie"' /> 
+            <div class='menu-btn' @click="logOut" :text='"Log out"' >Log out</div> <div class="separator"> · </div>
+            <div class='menu-btn' @click="this.$router.push('/profile')">Profile</div> <div class="separator"> · </div>
+            <div class='menu-btn' @click="this.$router.push('/')">Home</div> <div class="separator"> · </div>
+            <div class='menu-btn' @click="logOut">Movies</div>
         </div>
     </div>
 </template>
@@ -36,16 +37,40 @@ export default {
 
 <style scoped>
 #menu{
-    position: absolute; 
-    top: 0;
+  position: fixed; 
+  bottom: 0;
+  width: 99vw;
+  background-color: rgba(29, 24, 24, 0.144);
+  padding-left: 38%;
+  z-index: 999;
+  opacity: 1;
+  transition-duration: 200ms;
+}
+#menu:hover{
+  transition-duration: 200ms;
+  background-color: rgb(29, 24, 24);
 }
 #box{
   display: flex;
   justify-content: left;
   align-items: center;
+  text-align: center;
   flex-flow: row;
+  font-weight: lighter;
+  letter-spacing: 1.5px;
+}
+.separator{
+  margin-left: 5px;
+  margin-right: 5px;
+  font-size: 30px;
 }
 .menu-btn{
   margin: 5px;  
+  font-size: 14px;
+  color: rgb(161, 108, 108);
+}
+.menu-btn:hover{
+  cursor: pointer;
+  color: white;
 }
 </style>
