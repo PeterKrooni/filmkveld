8a22bd74ca548716872e585196547306c6738aeb
Jørgen Lohne <jorgen.lohne99@gmail.com>
2022-03-24 22:32:30 +0100

    validate date on movie runtime

--- src/backend/helper/sanitizers.js
+++
@@ -1,8 +0,0 @@
-function movieDateSanitizer(inputDate) {
-    dateObject = new Date(0);
-    try {
-        date.prototype.setUTCHours()
-    } catch (error) {
-        throw("not a proper date: " + error)
-    }
-}
