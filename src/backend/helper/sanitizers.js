2c82dfd0765f161c917154e561db9d46d97f40c6
Jørgen Lohne <jorgen.lohne99@gmail.com>
2022-03-24 21:34:25 +0100

    started work on date sanitization function

---
+++ src/backend/helper/sanitizers.js
@@ -0,0 +1,8 @@
+function movieDateSanitizer(inputDate) {
+    dateObject = new Date(0);
+    try {
+        date.prototype.setUTCHours()
+    } catch (error) {
+        throw("not a proper date: " + error)
+    }
+}
