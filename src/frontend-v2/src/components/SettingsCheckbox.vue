3f860dbaac3f2a42466dfdd486d5c4622ebbb25f
Dusan Nikolic <dusan1000@gmail.com>
2022-05-19 23:14:58 +0200

    Created settings component

---
+++ src/frontend-v2/src/components/SettingsCheckbox.vue
@@ -0,0 +1,44 @@
+<template>
+    <label for="myToggleButton">
+        {{this.settingName}}
+        <span v-if="isActivated">On</span>
+        <span v-else>Off</span>
+
+        <input type="checkbox" id="myToggleButton" v-model="valueChecker">
+        <span class="switcher"></span>
+    </label>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            buttonState: this.defaultState
+        }
+    },
+    computed: {
+        isActivated() { // activates the current state of the checkbox
+            console.log("CurrentState of button is", this.buttonState)
+            return this.buttonState 
+        },
+        valueChecker: {  // updates the value after the elem has been clicked
+            set(newState) {
+                this.buttonState = newState;
+                this.$emit("change", newState) // makes value available for parent component
+            }
+        }
+    },
+    props: {
+        settingName: "",
+        defaultState: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
