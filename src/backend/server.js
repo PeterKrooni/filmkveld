ac6b14db918baba5f1e95dfe5cdbab3cff356e5d
Peter <peter>
2022-05-22 14:19:15 +0200

    increase request body size limit in backend to enable sending bigger profile images (up to 5mb)

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,8 +25,8 @@ const port = process.env.PORT || 8000
 connectDB()
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({ extended: false, limit: '5mb' }))
 app.use(errorHandler)
 
 const crudPath = "/crud/api/"
