a4f053eb94f5f51048bcc80bef875da5c4ec6f4e
Peter <peter>
2022-06-12 12:32:05 +0200

    improve image cdn stuff in profile

--- src/frontend-v2/src/views/Profile.vue
+++ src/frontend-v2/src/views/Profile.vue
@@ -5,7 +5,7 @@
             <div id="ns">
                 <div id="profile-info">
                     <div id="profile-img">
-                        <img :src="imgSource" @click="openFileSelector" id="PP" alt="">
+                        <img :src="imgSource" @click="openFileSelector" id="PP" alt="max 5mb">
                         <input @change="updateProfilePicture" type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" style="display: none;">
                     </div>
                     <div id="profile-details">
@@ -19,7 +19,6 @@
                     <div><SmallHeader :toptext="'Your karmascore is ' + this.karma + '!'" :bottomtext="'True popularity!'" /></div>
                 </div>
             </div>
-            Settings: show tags by default, show created by default: 
             <div style="margin-top: 150px; margin-right: 35x; margin-bottom: -50px;"> <BigHeader :text="'Suggestions'"/> </div>
             <div id="suggestions">
                 <div id="suggestions-container" v-if="loaded">
@@ -75,13 +74,19 @@ export default {
     },
     methods: {
         async updateProfilePicture(event) {
+            alert("todo: remove existing images from cdn, fix that it updates, feedback if image too large")
             if (event.target.files.length > 0){
                 const b64_img = await this.readFileAsDataURL(event.target.files[0])
                 const me = await getMe()
                 const updated = await apiUpdateProfilePicture(me.userid, b64_img) 
-
-                console.log(updated)
-                this.imgSource = updated.profile_picture;
+                .then((res) => {
+                    console.log("picture updated!", res)
+                    this.imgSource = res.profile_picture;
+                    console.log(this.imgSource)
+                })
+                .catch((err) => {
+                    console.log("Failed to update profile picture", err)
+                })
             }
         },
         async readFileAsDataURL(file) {
