06bbafbf362114885e4aee76ad50e73669e2340d
Peter <peter>
2022-06-22 13:59:38 +0200

    forgot discord mapper in previous commit

--- src/frontend-v2/src/helpers/discordmapper.js
+++ src/frontend-v2/src/helpers/discordmapper.js
@@ -6,11 +6,23 @@
 
  import axios from 'axios'
 
-export async function getDiscordId(token){
-    const res = axios.get('https://discord.com/api/users/@me',{
+export async function getDiscordInformation(access_token, token_type){
+    const info = axios.get('https://discord.com/api/users/@me',{
         headers: {
-            'Authorization': token
+            'Authorization': ` ${token_type} ${access_token}`
         }
     })
-    console.log(res)
+    .then((res) => {
+        if (res.data.avatar){
+            res.data.avatar = `https://cdn.discordapp.com/avatars/${res.data.id}/${res.data.avatar}` 
+        } else{
+            res.data.avatar = "https://seeklogo.com/images/D/discord-color-logo-E5E6DFEF80-seeklogo.com.png" //todo add this to cloudinary
+        }
+        return res
+    })
+    .catch((err) => {
+        console.log(`Failed to get Discord account information: ${err}`)
+    })
+    return info
+
 }
