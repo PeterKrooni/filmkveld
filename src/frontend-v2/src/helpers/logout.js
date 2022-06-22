56d31de5f092828b2c9bfbe2b210a80b99c25bf9
Peter <peter>
2022-06-22 18:17:20 +0200

    add oauth cookie to logout

--- src/frontend-v2/src/helpers/logout.js
+++ src/frontend-v2/src/helpers/logout.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookies'
 export function logout(){
     axios.defaults.headers.common['Authorization'] = undefined
     Cookies.removeItem("jwt")
+    Cookies.removeItem("oauth")
 }
 
 export default logout;
